fa390ef19b60911e082300aefb47965153642802
tuanpham01 <tuan.pham@peahoki.com>
2022-09-25 12:58:35 -0700

    fix 3_Longest_Substring_Without_Repeating_Characters.c

--- 3_Longest_Substring_Without_Repeating_Characters.c
+++ 3_Longest_Substring_Without_Repeating_Characters.c
@@ -34,44 +34,72 @@ Constraints:
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int lengthOfLongestSubstring(char * s);
 
 //using counting sort idea
-int lengthOfLongestSubstring(char * s){
+int lengthOfLongestString(char * s, int len){
     char *temp = s;
-    char min = 0, max = 0;
-
-    while (*temp != '\0'){
+    int count = len;
+    char min = *temp, max = *temp;
+    
+    while (count-- > 0){
         if (*temp > max)
             max = *temp;
         if (*temp < min)
             min = *temp;
-	temp = temp+1;
-    }
-
-    temp = s;
-    int len = max - min + 1;
-    char arr[len];
-    for (int i = 0; i < len; i++)
-        arr[i] = 0;
-
-    while (*temp != '\0'){
-        arr[(int)(*temp -min)] += 1;
         temp = temp+1;
     }
 
+    temp = s;
+    count = len;
+    int size = max - min + 1;
+    int arr[size];
+    for (int i = 0; i < size; i++)
+        arr[i] = 0;
+    
+    while (count-- > 0){
+        arr[*temp -min] += 1;
+        temp = temp+1;
+    }
+   
     int ret = 0;
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < size; i++){
         if (arr[i] != 0)
             ret ++;
-
+    }
+    
     return ret;
+}
+
+int lengthOfLongestSubstring(char * s){
+    if (!strcmp(s, ""))
+        return 0;
+    int max = lengthOfLongestString(s, strlen(s));
+    fprintf(stdout, "max:: %d\n", max);
+    char * temp = s;
+    for (int i = max; i > 1; i--){
+        while (*(temp+i-1) != '\0'){
+            if (i == lengthOfLongestString(temp, i)){
+		fprintf(stdout, "substring:: %s\n", temp);
+                return i;
+            } 
+            //have repeat 
+            temp = temp+1;
+        }
+	temp = s;
+    }
+    
+    return 1;
 }
 
 
 void main(){
-    char* string = "abcabcbb";
+    //char* string = "abcabcbb";
+    char* string = "pwwkew";
+    //char* string = "au";
     fprintf(stdout, "length:: %d\n", lengthOfLongestSubstring(string));
+    //fprintf(stdout, "length:: %d\n", lengthOfLongestString(string, strlen(string)));
     return;
 }
